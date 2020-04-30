3bd7e00a6b8787e555c26d57575eb84b79ac5cc4
Jens Stavnstrup <stavnstrup@gmail.com>
2020-04-30 22:51:20 +0200

    Add company name to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,9 @@ import React from "react"
 import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
-  return <div className={footerStyles.footer}>Copyright © 2020</div>
+  return (
+    <div className={footerStyles.footer}>Copyright © 2020 SPROGLÆRERNE</div>
+  )
 }
 
 export default Footer
