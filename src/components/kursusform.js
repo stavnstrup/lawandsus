77176393671be6a34c5afd0a9e94903b01440aac
Jens Stavnstrup <stavnstrup@gmail.com>
2020-06-14 22:41:18 +0200

    Add submit button

--- src/components/kursusform.js
+++ src/components/kursusform.js
@@ -188,6 +188,15 @@ const KursusForm = () => {
               />
             </div>
           </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary mr-2">
+              Register
+            </button>
+            <button type="reset" className="btn btn-secondary">
+              Reset
+            </button>
+          </div>
         </Form>
       )}
     </Formik>
